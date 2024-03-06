a264d87818011cf429c5351ad6f79aa85960821a
BitWonder <drake@morgancomputers.net>
2024-03-06 16:22:30 -0600

    remove get database for now

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 // Deno code goes here!
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+// database setup
+//const database = await Deno.openKv();
+
+const app = new Application();
+const router = new Router();
+const port = 1027;
+
+// router gets go here
+
+
+// setup initialize app
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(async (context) => {
+  await context.send({
+    root: `${Deno.cwd()}/`,
+    index: "./index.html",
+  });
+});
+
+console.log("Server is running");
+await app.listen({ port });
