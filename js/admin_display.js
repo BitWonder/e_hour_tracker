38ad3289e6a6bd65b59a4a8d891870289b95021f
BitWonder <drake@morgancomputers.net>
2024-03-08 17:47:47 -0600

    from next

--- js/admin_display.js
+++ js/admin_display.js
@@ -1,3 +1,3 @@
 function next(user_json) {
-    document.getElementById("academy").src = user_json.academy;
+    document.getElementById("academy").src = `../images/academy/${user_json.academy}.png`;
 }
