4a5394c783ccb31c6d71832eb9d90bb0ee0762ac
BitWonder <drake@morgancomputers.net>
2024-03-09 09:39:58 -0600

    import correct css

--- js/admin_display.js
+++ js/admin_display.js
@@ -1,3 +1,4 @@
 function next(user_json) {
     document.getElementById("academy").src = `../images/academy/${user_json.academy}.png`;
+    document.getElementById("welcome").innerText = `Welcome ${user_json.full_name}!`;
 }
