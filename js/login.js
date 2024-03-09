c648a3adca08456789cabdb9c9481fa3e87c3488
BitWonder <drake@morgancomputers.net>
2024-03-09 12:40:53 -0600

    new index

--- js/login.js
+++ js/login.js
@@ -31,7 +31,11 @@ document.getElementById("login").onsubmit = async function(event) {
         let user = await response.json()
         localStorage.setItem("user_id", user.id)
         if (user.user == "admin") {
-            window.location.href = "./admin/";
+            if (user.academy == "all") {
+                window.location.href = "./admin/";
+                return;
+            }
+            window.location.href = "./teacher/";
             return;
         }
         window.location.href = "./student/";
