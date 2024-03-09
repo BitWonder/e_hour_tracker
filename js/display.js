84d16185afd764e1c9c47610528cb0c996b81d15
BitWonder <drake@morgancomputers.net>
2024-03-09 17:08:01 -0600

    load js start

--- js/display.js
+++ js/display.js
@@ -1,8 +1,11 @@
+function next(user_json) {
+    document.getElementById("academy").src = `../images/academy/${user_json.academy}.png`;
+    document.getElementById("welcome").innerText = `Welcome ${user_json.full_name}!`;
+}
+
 // now for the formatting stuff
 function continue_function() {
     let user = JSON.parse(json_user);
-    document.getElementById("welcome").innerText = `Welcome, ${user.full_name}!`
-    document.getElementById("academy").src = `../images/academy/${user.academy}.png`
     let total_hours = 0;
     user.hours.forEach(element => {
         total_hours += element.hours
