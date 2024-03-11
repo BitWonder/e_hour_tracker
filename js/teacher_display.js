1f182ce85560f2d994d7ba9f0ff3bb8ac30d4a2a
BitWonder <drake@morgancomputers.net>
2024-03-11 11:58:39 -0500

    long take

--- js/teacher_display.js
+++ js/teacher_display.js
@@ -28,7 +28,7 @@ async function next(user_json) {
             console.log(student);
             let total_hours = 0;
             for (let request in student.requested) {
-                total_hours += parseFloat(request.amount);
+                total_hours += parseFloat(student.requested[parseInt(request)].amount);
             }
             let a = document.createElement("a");
             a.href = "./student.html";
