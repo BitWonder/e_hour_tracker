88b14fa6fe150e0e50010b0f0a0ec1d53404edbf
BitWonder <drake@morgancomputers.net>
2024-03-15 08:52:28 -0500

    send as string

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ router.post("/login",            async (context) => {
         password: user.password
     }, { expireIn: 3600000 }); // a database linker that expires in one hour
     context.response.status = 200;
-    context.response.body = {user: uuid};
+    context.response.body = JSON.stringify({user: uuid});
     context.response.type = "json";
     return;
 });
