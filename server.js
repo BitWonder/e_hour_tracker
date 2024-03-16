58c21bfc288c188d3964fad264bd36b43c56cfc9
BitWonder <drake@morgancomputers.net>
2024-03-16 15:33:56 -0500

    fix primary key

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ router.post("/password", async (context) => {
     let user = (await database.get(["user", input.username, hashed_password])).value;
     await database.delete(["user", input.username, user.password]);
     await database.delete(["user", user.academy, input.username]);
-    primary_key = ["user", user.username, new_password]
+    let primary_key = ["user", user.username, new_password]
     let response = await database.atomic()
         .check({key: primary_key})
         .set(primary_key, user)
